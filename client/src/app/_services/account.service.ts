eb5b9b866d28e58750335c04104b0fbca390771d
Ross easterbrook <rossjohneast@hotmail.com>
2021-01-13 13:43:38 +0000

    End of section 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -19,6 +19,17 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) =>{
+        if (user){
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
+
   constructor() { }
 
   ngOnInit(): void {
@@ -15,6 +17,10 @@ export class HomeComponent implements OnInit {
 
   registerToggle(){
     this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean){
+    this.registerMode = event;
   }
 
 }
